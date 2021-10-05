32654fc67d7325ef2a96ebf6e70721b3f2783cc7
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-10-05 16:06:34 +0700

    Modified beberapa file terutama MainActivity.kt dan AndroidManifest.xml solve add uses-sdk dengan probleb using-sdk:minSdkVersion 21 tidak boleh lebih kecil dari versi 24

--- app/src/main/java/com/nurkholiq/qr_code_zxing/MainActivity.kt
+++ app/src/main/java/com/nurkholiq/qr_code_zxing/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.nurkholiq.qr_code_zxing
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,14 +10,22 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
+import org.json.JSONException
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks,
+    EasyPermissions.RationaleCallbacks {
 
     var cardView1: CardView? = null
     var cardView2: CardView? = null
     var btnScan: Button? = null
     var btnEnterCode: Button? = null
+    var btnEnter: Button? = null
     var edtCode: EditText? = null
     var tvText: TextView? = null
     var hide: Animation? = null
@@ -29,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         cardView2 = findViewById(R.id.cv_qr2)
         btnScan = findViewById(R.id.btn_scan)
         btnEnterCode = findViewById(R.id.btn_enter_code)
+        btnEnter = findViewById(R.id.btn_enter)
         edtCode = findViewById(R.id.edt_code)
         tvText = findViewById(R.id.tv_title)
 
@@ -48,6 +59,21 @@ class MainActivity : AppCompatActivity() {
             cardView2!!.visibility = View.VISIBLE
             cardView1!!.visibility = View.GONE
             tvText!!.setText("Scan QR Code Here")
+
+        }
+
+        cardView2!!.setOnClickListener {
+            cameraTask()
+        }
+
+        btnEnter!!.setOnClickListener {
+            if (edtCode!!.text.toString().isNullOrEmpty()) {
+                Toast.makeText(this, "Plaese Enter Code", Toast.LENGTH_LONG).show()
+            } else {
+                var value = edtCode!!.text.toString()
+                Toast.makeText(this, value, Toast.LENGTH_LONG).show()
+
+            }
         }
 
         btnEnterCode!!.setOnClickListener {
@@ -60,5 +86,82 @@ class MainActivity : AppCompatActivity() {
             tvText!!.setText("Enter QR Code Here")
         }
 
+    }
+
+    private fun hasCameraAccess(): Boolean {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)
+    }
+
+    private fun cameraTask() {
+        if (hasCameraAccess()) {
+            val qrScanner = IntentIntegrator(this)
+            qrScanner.setPrompt("Scan a QR Code")
+            qrScanner.setCameraId(0)
+            qrScanner.setOrientationLocked(true)
+            qrScanner.setBeepEnabled(true)
+            qrScanner.captureActivity = CaptureActivity::class.java
+            qrScanner.initiateScan()
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs access to your camera so you can take pictures.",
+                123,
+                Manifest.permission.CAMERA
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        var result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show()
+                edtCode!!.setText("")
+            } else {
+                try {
+
+                    cardView1!!.startAnimation(reveal)
+                    cardView2!!.startAnimation(hide)
+
+                    cardView1!!.visibility = View.VISIBLE
+                    cardView2!!.visibility = View.GONE
+                    edtCode!!.setText(result.contents.toString())
+                } catch (exception: JSONException) {
+                    Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_LONG).show()
+                    edtCode!!.setText("")
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
     }
 }
